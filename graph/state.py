9cd4ec402b73758f9d3df1c793510ebb4fa3c01c
Anusha <ksanushakoti@gmail.com>
2026-03-19 15:41:22 +0530

    Initial setup

---
+++ graph/state.py
@@ -0,0 +1,5 @@
+from langgraph.graph import MessagesState
+
+class AgentState(MessagesState):
+    """""State class for agents to carry the coversation history and other relevant information."""
+    pass
